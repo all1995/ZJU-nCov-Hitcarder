3bc33a00c290790f82272474e0bb42669fc7f2b3
zju-ch <35737620+zju-ch@users.noreply.github.com>
2022-05-08 19:48:58 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,15 @@
+import requests
+import ddddocr
+
+main_url = 'https://healthreport.zju.edu.cn/ncov/wap/default/index'
+captcha_url = 'https://healthreport.zju.edu.cn/ncov/wap/default/code'
+ocr = ddddocr.DdddOcr()
+
+sess = requests.session()
+# 设置 cookie
+cookie_dict = {'eai-sess': 'q7t9nc9lb4fjtc1pb3n5b61cu4'}
+sess.cookies = requests.cookies.cookiejar_from_dict(cookie_dict)
+
+resp = sess.get(captcha_url)
+captcha = ocr.classification(resp.content)
+print(captcha)
